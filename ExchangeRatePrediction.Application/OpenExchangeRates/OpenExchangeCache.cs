dda1c8ad0aa1027e7594ae7e63a0634cad4f3850
NGUYEN Tan Phu <nphu@amaris.com>
2019-01-11 11:39:29 +0700

    Restructure cache

---
+++ ExchangeRatePrediction.Application/OpenExchangeRates/OpenExchangeCache.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ExchangeRatePrediction.Application.OpenExchangeRates
+{
+    public class OpenExchangeCache
+    {
+        public List<OpenExchangeRateResult> InmemoryData { get; set; } = new List<OpenExchangeRateResult>();
+    }
+}
